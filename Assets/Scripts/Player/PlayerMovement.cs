185afaac1ee26c010a870d42d2bcd5f6d956215d
DrPlague6 <drplague@tutanota.com>
2022-05-09 15:48:06 +0200

    Player Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    public PlayerController PlayerControllerScript;
+    public PlayerScript PlayerControllerScript;
     public float MovementSpeed;
     private float InputHorizontalAxis;
     private float InputVerticalAxis;
-    public Rigidbody PlayerRB;
+    private CharacterController playerController;
+    public Transform CameraTransform;
+    public float TurnSmoothingTime = 0.1f;
+    private float turnSmoothingVelocity;
     void Start(){
-        PlayerRB = GetComponent<Rigidbody>();
+        playerController = GetComponent<CharacterController>();
     }
     void Update(){
         InputVerticalAxis = Input.GetAxis("Vertical");
         InputHorizontalAxis = Input.GetAxis("Horizontal");
-        if(InputHorizontalAxis == 0)
+        Vector3 movementDirection = new Vector3(InputHorizontalAxis,0,InputVerticalAxis);
+        if(movementDirection.magnitude <= 0.01f)
             return;
-        else if(InputVerticalAxis == 0)
-            return;
-        PlayerRB.AddForce(transform.forward * InputVerticalAxis * MovementSpeed,ForceMode.Acceleration);
+            float targetAngle = Mathf.Atan2(movementDirection.x,movementDirection.z) * Mathf.Rad2Deg + CameraTransform.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothingVelocity,TurnSmoothingTime);
+            transform.rotation = Quaternion.Euler(0f,smoothAngle,0f);
+
+        movementDirection = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
+        playerController.Move(movementDirection.normalized * MovementSpeed * Time.deltaTime);
     }
 }
