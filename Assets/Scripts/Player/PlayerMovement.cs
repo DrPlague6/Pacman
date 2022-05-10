8293080a4ef50982214179b5a62acd1f58141680
DrPlague6 <drplague@tutanota.com>
2022-05-10 11:49:50 +0200

    PowerUps
    Health and Speed Boosters

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,12 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     public PlayerScript PlayerControllerScript;
-    public float MovementSpeed;
     private float InputHorizontalAxis;
     private float InputVerticalAxis;
     private CharacterController playerController;
     public Transform CameraTransform;
-    public float TurnSmoothingTime = 0.1f;
     private float turnSmoothingVelocity;
     void Start(){
         playerController = GetComponent<CharacterController>();
@@ -23,10 +21,10 @@ public class PlayerMovement : MonoBehaviour
         if(movementDirection.magnitude <= 0.01f)
             return;
             float targetAngle = Mathf.Atan2(movementDirection.x,movementDirection.z) * Mathf.Rad2Deg + CameraTransform.eulerAngles.y;
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothingVelocity,TurnSmoothingTime);
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothingVelocity, PlayerControllerScript.TurnSmoothingTime);
             transform.rotation = Quaternion.Euler(0f,smoothAngle,0f);
 
         movementDirection = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
-        playerController.Move(movementDirection.normalized * MovementSpeed * Time.deltaTime);
+        playerController.Move(movementDirection.normalized * PlayerControllerScript.PlayerSpeed * PlayerControllerScript.SpeedBoost * Time.deltaTime);
     }
 }
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,32 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-    public int Health;
+    public int MaxHealth = 3;
+    private int health;
     public float PlayerSpeed;
+    public float TurnSmoothingTime = 0.1f;
+
+    public Canvas HealthBar;
+    public Sprite hpSprite;
+    public int score;
+    public float SpeedBoost = 1;
+    public float BoostersDuration = 100;
+    private float boosterTimeExp = 0;
     void Start()
     {
-        
+        //add hp icons to bar
+        for(int i =0; i<MaxHealth;i++){
+           createNewHpIcon();
+        }
+        health = MaxHealth;
     }
 
     void Update()
     {
-        
+        if(Time.time > boosterTimeExp){
+            SpeedBoost = 1;
+        }
     }
     public void SpeedPowerUP(){
-        PlayerSpeed *= 1.5f;
+        SpeedBoost = 2;
     }
     public void HealthPowerUP(){
-        Health += 1;
+        if(health++ >= MaxHealth){
+            score +=10;
+        }
+        else{
+            health++;
+            createNewHpIcon();
+        }
+        
+    }
+    private void createNewHpIcon(){
+        GameObject hpIcon = new GameObject();
+        Image newHpImage = hpIcon.AddComponent<Image>();
+        newHpImage.sprite = hpSprite;
+        hpIcon.transform.SetParent(HealthBar.transform);
     }
     public void takeDamage(){
-        Health--;
-        if(Health ==0)
+        health--;
+        if(health == 0)
             die();
     }
     private void die(){
         //TODO:: ENDGAME SCREEN
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.layer == LayerMask.NameToLayer("Collectables"))
+        {
+            switch (other.gameObject.tag){
+                case "HealthBoost":
+                    HealthPowerUP();
+                    break;
+                case "SpeedBoost":
+                    SpeedPowerUP();
+                    boosterTimeExp = Time.time + BoostersDuration;
+                    break;
+                default:
+                    score++;
+                    break;
+            }
+            Destroy(other.gameObject);
+            Debug.Log("playerhit");
+        }
     }
 }
